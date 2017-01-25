1edc85d2266ee90adfec2a626e40742c60ca9039
nhc <fc@sz>
2017-01-25 11:10:00 +0800

    include broadcast

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -7,14 +7,51 @@ use Illuminate\Http\Request;
 
 class WechatController extends Controller
 {
-    public function index()
+    private $app;
+
+    public function __construct()
     {
         $cfg = [
             'app_id' => 'wx3db2aee0760bd36a',
             'secret' => '8f0470faa641cb07ed9273092c095587',
             'token'  => 'smallbaby',
+
+            'debug'  => true,
+            'log'    => [
+                'level' => 'debug',
+            ]
         ];
-        $app = new Application($cfg);
-        return $app->server->serve();
+
+        $this->app = new Application($cfg);
+    }
+
+    public function index()
+    {
+        $server = $this->app->server;
+        $server->setMessageHandler(function($message){
+           switch ($message->MsgType) {
+               case 'event':
+                   break;
+               case 'text':
+                   break;
+               case 'image':
+                   break;
+               case 'voice':
+                   break;
+               case 'video':
+                   break;
+               case 'location':
+                   break;
+               case 'link':
+                   break;
+               default:
+           }
+        });
+        return $server->serve();
+    }
+
+    public function broadcast()
+    {
+        $broadcast = $this->app->broadcast;
     }
 }
